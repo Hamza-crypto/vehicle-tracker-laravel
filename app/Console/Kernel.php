bbad7f387af210b966275b9d684e784a7cc8ed45
hamza-crypto <6793sidique@gmail.com>
2023-06-02 14:26:44 +0500

    Cron job time increased

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('guru:token-refresh')
-            ->everyThirtyMinutes();
+            ->hourly();
 
         $schedule->command('guru:fetch-new-jobs')
             ->everyFiveMinutes();
