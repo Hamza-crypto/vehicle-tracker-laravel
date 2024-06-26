c29fd575e11504f5906bee76503fd010af2adc25
Hamza <6793siddique@gmail.com>
2024-06-26 08:50:03 +0500

    asdsa

--- routes/web.php
+++ routes/web.php
@@ -171,3 +171,5 @@ Route::get('store-jobs', [GuruController::class, 'store_jobs']);
 
 Route::get('get_run_lists', [RunListController::class, 'get_run_lists'])->name('runlists.filter');
 Route::get('export_run_list', [RunListController::class, 'export_run_list'])->name('runlist.export');
+
+//
