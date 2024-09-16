4e7da19ee67f05e5f022eb51b7f7f1b0be8de2a3
Hamza <6793siddique@gmail.com>
2024-09-16 20:28:46 +0500

    Changes from server

--- resources/views/pages/dashboard/_inc/sold-table.blade.php
+++ resources/views/pages/dashboard/_inc/sold-table.blade.php
@@ -2,9 +2,9 @@
     <div class="col-12 col-lg-6 d-flex">
         <div class="card flex-fill">
             <div class="card-header">
-                <h5 class="card-title mb-0">Days in Yard</h5>
+                <h3 class="mb-2"> Most Days at Auction</h3>
             </div>
-            <table class="vehicles-table table table-sm table-striped my-0">
+            <table class="vehicles-table table table-sm table-striped table-hover my-0">
                 <thead>
                     <tr>
                         <th>Year Make Model</th>
@@ -18,8 +18,8 @@
                             <tr id="{{ $vehicle->id }}">
                                 <td><a href="#" data-toggle="modal"
                                         data-target="#modal-vehicle-detail">{{ $vehicle->description }}</a></td>
-                                <td><a href="#" data-toggle="modal"
-                                        data-target="#modal-vehicle-detail">{{ $vehicle->vin }}</a></td>
+                                <td><a href="https://www.copart.com/lot/{{ $vehicle->auction_lot }}"
+                                        target="_blank">{{ $vehicle->vin }}</a></td>
                                 <td><a href="#" data-toggle="modal"
                                         data-target="#modal-vehicle-detail">{{ $vehicle->meta_value }}</a></td>
                             </tr>
@@ -36,9 +36,9 @@
     <div class="col-12 col-lg-6 d-flex">
         <div class="card flex-fill">
             <div class="card-header">
-                <h5 class="card-title mb-0">Recently Sold Vehicles</h5>
+                <h3 class="mb-2">Recently Sold Vehicles</h3>
             </div>
-            <table class="vehicles-table table table-sm table-striped my-0">
+            <table class="vehicles-table table table-sm table-striped table-hover my-0">
                 <thead>
                     <tr>
                         <th>Year Make Model</th>
